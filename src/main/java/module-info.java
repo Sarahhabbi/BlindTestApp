5100297091957d303583f6c614bc52a68de28555
Sarahhabbi <habbisarah.1@gmail.com>
2022-01-06 21:03:47 +0100

    tested and debug maven dependency

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,9 +3,12 @@ module com.example.demo {
     requires javafx.fxml;
     requires javafx.web;
 
-
-
-
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
     requires javafx.graphics;
     requires java.sql;
 
--- src/main/java/repositories/RepositoryFactory.java
+++ src/main/java/repositories/RepositoryFactory.java
@@ -5,7 +5,7 @@ import models.MyImage;
 
 public class RepositoryFactory {
 
-    private static final Database DATABASE = Database.getInstance("jdbc:mysql://localhost:3306/blind_test", "root", "Siyani=17");
+    private static final Database DATABASE = Database.getInstance("jdbc:mysql://localhost:3306/blind_test", "root", "poudebs91");
 
     public static Repository<MyImage> images() {
         return new CompositeRepository<>(MyImageRepository.getInstance(DATABASE.getConnection()));
